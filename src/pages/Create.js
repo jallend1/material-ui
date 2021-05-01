335add7779c2b6f4658c9908db855c3ceabd78c3
jallend1 <jallend1@gmail.com>
2021-05-01 12:17:39 -0700

    Builds create note form asnd implements basic error handling and styling for title and details

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,32 @@
-import { Container, Typography, Button, makeStyles } from '@material-ui/core/'
+import { useState } from 'react';
+import { Container, Typography, Button, TextField, makeStyles } from '@material-ui/core/'
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 const useStyles = makeStyles({ 
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
 })
 
 export default function Create() {
   const classes = useStyles();
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+    if(title === '') setTitleError(true);
+    if(details === '') setDetailsError(true);
+    if(title && details){
+      console.log(title, details)
+    }
+  }
   return (
     <Container>
       <Typography
@@ -17,15 +38,38 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField 
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField 
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          multiline
+          rows={4}
+          error={detailsError}
+        />
       <Button 
         className={classes.btn}
-        onClick={() => console.log('Clicked!!!')} 
         endIcon={<KeyboardArrowRightIcon />}
         type="submit" 
         color="secondary" 
         variant="contained">
           Submit
       </Button>
+      </form>
     </Container>
   )
 }
