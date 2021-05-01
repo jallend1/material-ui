4917cbba90a43dd7d9e5b5e5700d3ed00ae6e197
jallend1 <jallend1@gmail.com>
2021-05-01 13:20:25 -0700

    Implements grid system for notes display

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,15 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import { Container, Grid, Paper } from '@material-ui/core';
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:8000/notes').then(res => res.json()).then(data => setNotes(data))
-  }, [])
+    fetch('http://localhost:8000/notes')
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, []);
+
   return (
-    <div>
-      {notes.map(note => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
-  )
+    <Container>
+      <Grid container>
+        {notes.map((note) => (
+          <Grid item key={note.id} xs={12} md={6} lg={4}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
