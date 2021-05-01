27b33828d7af0fb89c6ac422b8da646bd3d3c9e4
jallend1 <jallend1@gmail.com>
2021-05-01 11:43:24 -0700

    Imports icons and implements some sample custom styles

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import { Container, Typography, Button, makeStyles } from '@material-ui/core/'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20
+  }  
+})
 
 export default function Create() {
+  const classes = useStyles();
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+        className={classes.title}
+        variant="h6"
+        component="h2"
+        color="textSecondary"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+      <Button 
+        className={classes.btn}
+        onClick={() => console.log('Clicked!!!')} 
+        endIcon={<KeyboardArrowRightIcon />}
+        type="submit" 
+        color="secondary" 
+        variant="contained">
+          Submit
+      </Button>
+    </Container>
   )
 }
